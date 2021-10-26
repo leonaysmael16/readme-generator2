0de8011c06993701ac635b309df0008d6217c4fc
leonaysmael16 <ysmealleona@gmail.com>
2021-10-25 22:27:23 -0700

    added more to the code

--- index.js
+++ index.js
@@ -1,72 +1,143 @@
 // Reference source: https://stackoverflow.com/questions/62597209/trying-to-generate-a-readme-using-nodejs
 
 // TODO: Include packages needed for this application
-const generateMarkdown = require(".util/generatemarkdown.js");
+const generateMarkdown = require("./util/generatemarkdown");
 const { fstat } = require("fs");
-const inquire = require("inquire");
-const axios =require("axios");
+// const inquire = require("inquirer");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type:"input",
         message:"What's the title of your project?",
-        Name: "title"
+        Name: "title",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please input title');
+                return false;
+            }
+        }
         
     },
     {
         type: "Input",
         Message: "Give a detailed description about your project",
-        Name: "Description"
+        Name: "Description",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true; 
+            } else{
+                console.log('Please write description');
+                return false;
+            }
+
+        }
     },
     {
     type: "Input",
     Message: "What's the purpose of this project?",
-    Name: "Purpose"
-    },
+    Name: "Purpose",
+    validate: purposeInput => {
+        if (purposeInput) {
+            return true;
+        } else {
+            console.log('Please input purpose of the project')
+            return false;
+        }
+    }
+
+    }
+    ,
     
     
     {
         type: "input",
         Message: "What did you need to install or do to run this project?",
-        Name: "Installation or Tools"
-        
+        Name: "Installation or Tools",
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please insert installation process of none, N/A')
+                return false;
+            }
+        }
     }
     ,
     {
         type: "input",
         Message: "What kind of license?",
-        Name: "License"
+        Name: "License",
+        validate: licenseInput => {
+            if (licenseInput)  {
+                return true;
+            } else {
+                console.log('Please enter type of license')
+                return false;
+            }
+        }
 
     },
     {
         type: "input",
         Message: "who contributed to this project?",
-        Name: "Contributions"
+        Name: "Contributions",
+        validate: contributionsInput => {
+            if (contributionsInput) {
+                return true;
+            } else {
+                console.log('Please input contributors on the project if not, N/A')
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        Message: "What is your GitHub username?",
+        Name: 'Github username',
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            } else {
+                console.log('Please enter your github username')
+                return false;
+            }
+        }
     }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeToFile(filename, date, err => {
-if (err) {
-    return console.log(err);
-}
-console.log('Your file has been created')
-});
-    }
-
+    fs.writeToFile(filename, data, (err) => {
+if (err) 
+        throw err;
+    console.log('Successfully store into your README file')
+    })
+}    
 // TODO: Create a function to initialize app
 function init() {
     inquire.prompt(questions)
-    .then((inquireResponse, data) => {
-        console.log("Make README");
-        fs.writeFileSync("README.md", inquireResponse, data);
+    .then(function (userInput) {
+        console.log(userInput);
+        writeToFile("README.md", generateMarkdown(userInput));
     })
-    .catch((err) => {
-        console.log(err);
-    })
+//     .catch((err) => {
+//         console.log(err);
+//     })
 }
+
+// const init = async () => {
+//     try {
+//         await inquirer.prompt('Welcome to the README Generator');
+//         const data = await inquirer.prompt(questions);
+//         writeToFile('README.md', generateMarkdown(data));
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
 
 // Function call to initialize app
 init();
